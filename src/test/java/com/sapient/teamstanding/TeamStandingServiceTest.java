090e6cb3891d37f611146e9e6671f572f8e0e187
Abhishek <sawhney.abhishek1983@gmail.com>
2020-05-23 17:06:42 +0530

    Pipeline file and dummy test

--- src/test/java/com/sapient/teamstanding/TeamStandingServiceTest.java
+++ src/test/java/com/sapient/teamstanding/TeamStandingServiceTest.java
@@ -2,11 +2,17 @@ package com.sapient.teamstanding;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.sapient.teamstanding.service.TeamStandingService;
 
 @RunWith(SpringRunner.class)
 public class TeamStandingServiceTest {
 
+	@Autowired
+	TeamStandingService service;
+	
 	@Test
 	public void givenCountryNameReturnsStandings() {
 		
